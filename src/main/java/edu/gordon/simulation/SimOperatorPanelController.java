22cedd65221883bd591be74da632cd5523ffa4f4
César Jeanroy <cesar.jeanroy@gmail.com>
2016-01-20 11:06:23 -0500

    Exemple of sepatation of the logic from the view.

---
+++ src/main/java/edu/gordon/simulation/SimOperatorPanelController.java
@@ -0,0 +1,69 @@
+package edu.gordon.simulation;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * The controller class of the SimOperatorPanel
+ */
+public class SimOperatorPanelController {
+
+
+    /**
+     * Constructor
+     * @param panel The panel linked to that controller
+     */
+    public SimOperatorPanelController (final SimOperatorPanel panel) {
+
+        final Button button = panel.getButton();
+        final Label  message = panel.getMessage();
+        final Simulation simulation = panel.getSimulation();
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                if (button.getLabel().equals("OFF"))    // ATM is currently on
+                {
+                    message.setText("Click button to turn ATM on  ");
+                    button.setLabel(" ON ");
+
+                    simulation.switchChanged(false);
+                }
+                else                                    // ATM is currently off
+                {
+                    message.setText("Click button to turn ATM off");
+                    button.setLabel("OFF");
+
+                    simulation.switchChanged(true);
+                }
+            }
+        });
+
+        // Use a thread to blink the "Click button to turn ATM on" message when
+        // the ATM is off.  This will also make the message invisible when the
+        // button is not enabled.
+
+        new Thread() {
+            public void run()
+            {
+                while(true)
+                {
+                    try
+                    {
+                        sleep(1000);
+                    }
+                    catch(InterruptedException e)
+                    { }
+
+                    if (message.isVisible() && ! button.getLabel().equals("OFF")
+                            || ! panel.isEnabled() )
+                        message.setVisible(false);
+                    else
+                        message.setVisible(true);
+                }
+            }
+        }.start();
+    }
+
+}
